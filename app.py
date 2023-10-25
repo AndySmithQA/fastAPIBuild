85cda05dc682b7dced77d56d59c25d29c4aea5c8
Andy Smith <andy.smith@qa.com>
2023-10-25 10:09:07 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+#run with:  uvicorn app:app --reload
+
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
+
+app = FastAPI()
+
+@app.middleware("http")
+async def medify_request_response_middleware(request: Request, call_next):
+    request.scope["path"] = str(request.url.path).replace("api", "apiv2")
+    response = await call_next(request)
+    if isinstance(response, StreamingResponse):
+        response.headers["X-Custom-Header"] = "Modified"
+    return response
+
+@app.get("/info")
+async def hello():
+    return {"message":"Hello, World"}
+
+@app.get("/apiv2/info")
+async def hellov2():
+    return {"Message":"Hello, world from V2"}
